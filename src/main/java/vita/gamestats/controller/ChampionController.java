0574602ae02ab63b7bd56155f73698d51dd44776
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-09-05 00:54:55 +0200

    basic fe

---
+++ src/main/java/vita/gamestats/controller/ChampionController.java
@@ -0,0 +1,28 @@
+package vita.gamestats.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vita.gamestats.model.Champion;
+import vita.gamestats.service.ChampionService;
+
+
+@RestController
+@RequestMapping("/api/champion")
+public class ChampionController {
+
+    @Autowired
+    private ChampionService championService;
+
+    @GetMapping
+    public ResponseEntity<List<Champion>> getAllChampions() {
+        return ResponseEntity.ok(championService.getAllChampions());
+    }
+    
+    
+}
---
+++ src/main/java/vita/gamestats/service/ChampionService.java
@@ -0,0 +1,21 @@
+package vita.gamestats.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import vita.gamestats.model.Champion;
+import vita.gamestats.repository.ChampionRepository;
+
+@Service
+public class ChampionService {
+    
+    @Autowired
+    private ChampionRepository championRepository;
+
+    public List<Champion> getAllChampions() {
+        return championRepository.findAll();
+    }
+
+}
