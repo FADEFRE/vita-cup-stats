0a6167946e8c2174fdac39c029545e089fc4b862
Frederik Kluge <fk13mami@studserv.uni-leipzig.de>
2024-09-04 17:35:26 +0200

    first commit

---
+++ src/main/java/vita/gamestats/GamestatsApplication.java
@@ -0,0 +1,13 @@
+package vita.gamestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamestatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamestatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vita/gamestats/GamestatsApplicationTests.java
@@ -0,0 +1,13 @@
+package vita.gamestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
